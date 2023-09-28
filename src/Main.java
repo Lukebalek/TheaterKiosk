f779c9d68c643c4d84808f138721627630e00f1e
Lukebalek <Lukebalek23@gmail.com>
2023-09-28 15:25:41 -0400

    Kiosk

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter their age
         System.out.print("Please enter your age: ");
         int age = scanner.nextInt();
 
-        // Check if the age is 21 or older and display a message accordingly
         if (age >= 21) {
             System.out.println("Congratulations! You get a wristband.");
         }
